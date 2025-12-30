ea972bf41d4e1a781703ced56f362aee36dd79f1
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2025-12-30 10:14:42 +0000

    change index title

--- quiz-worker-py/src/templates/index_template.py
+++ quiz-worker-py/src/templates/index_template.py
@@ -285,7 +285,7 @@ def INDEX_TEMPLATE(
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Location Map</title>
+        <title>Pub Quiz Map</title>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
         <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
         <link rel="stylesheet" href="https://unpkg.com/@geoapify/leaflet-address-search-plugin@^1/dist/L.Control.GeoapifyAddressSearch.min.css" />
